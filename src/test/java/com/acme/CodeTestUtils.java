a970a23e66b0f1877df0b94c8a21c958684cad74
Trejkaz (pen name) <trejkaz@trypticon.org>
2017-05-31 16:07:19 +1000

    Initial commit.

---
+++ src/test/java/com/acme/CodeTestUtils.java
@@ -0,0 +1,17 @@
+package com.acme;
+
+/**
+ * Source code test utils.
+ */
+public class CodeTestUtils
+{
+    /**
+     * Does stuff
+     *
+     * @throws Exception if an error occurs.
+     */
+    public static void checkProject() throws Exception
+    {
+        // empty
+    }
+}
