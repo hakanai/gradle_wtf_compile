9edc29ae0f4aad7a2aeba6e6390db0b634995538
Trejkaz (pen name) <trejkaz@trypticon.org>
2017-06-01 09:33:20 +1000

    Happens outside of tests too

---
+++ src/main/java/com/acme/Utils.java
@@ -0,0 +1,39 @@
+package com.acme;
+
+import java.io.IOException;
+
+/**
+ * Example utility class.
+ */
+public class Utils
+{
+    /**
+     * Does stuff
+     *
+     * @throws Exception if an error occurs.
+     */
+    public static void checkProject1() throws Exception
+    {
+        // empty
+    }
+
+    /**
+     * Does stuff
+     *
+     * @throws RuntimeException if an error occurs.
+     */
+    public static void checkProject2() throws Exception
+    {
+        // empty
+    }
+
+    /**
+     * Does stuff
+     *
+     * @throws IOException if an error occurs.
+     */
+    public static void checkProject3() throws IOException
+    {
+        // empty
+    }
+}
